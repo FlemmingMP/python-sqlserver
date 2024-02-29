4fc1932843f4d0ad134339bbda4e5637ace3f88d
Simen A. W. Olsen <simen@bjerk.io>
2024-02-29 14:34:46 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -9,7 +9,7 @@ database_port = os.environ.get("DATABASE_PORT") or "1433"
 print("==== Connecting to ", database_host)
 
 # Tveit-instans, VB - SQLAlchemy:
-sql_connection_string_FPT_TV_SQL01_VB = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={database_host},{database_port};DATABASE=master;UID=sa;PWD={database_password};Encrypt=no"
+sql_connection_string_FPT_TV_SQL01_VB = f"DRIVER={{SQL Server}};SERVER={database_host},{database_port};DATABASE=master;UID=sa;PWD={database_password};Encrypt=no"
 
 sql_connection_url_FPT_TV_SQL01_VB = URL.create(
     "mssql+pyodbc", query={"odbc_connect": sql_connection_string_FPT_TV_SQL01_VB})
