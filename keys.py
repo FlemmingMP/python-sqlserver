299d09245a643a55716e07c9f0be2d3f2f1163c4
Simen A. W. Olsen <so@bjerk.io>
2024-02-29 10:22:22 +0200

    add app to docker-compose and use docker network host in connection string

--- keys.py
+++ keys.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine, URL
 
 # Tveit-instans, VB - SQLAlchemy:
-sql_connection_string_FPT_TV_SQL01_VB = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost,1433;DATABASE=master;UID=sa;PWD=Qwerty123;Encrypt=no"
+sql_connection_string_FPT_TV_SQL01_VB = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=db,1433;DATABASE=master;UID=sa;PWD=Qwerty123;Encrypt=no"
 
 sql_connection_url_FPT_TV_SQL01_VB = URL.create(
     "mssql+pyodbc", query={"odbc_connect": sql_connection_string_FPT_TV_SQL01_VB})
