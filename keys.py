d7f06652d3dd79e128fedee9f69a669ea13c30cf
Simen A. W. Olsen <simen@bjerk.io>
2024-02-29 11:19:31 +0200

    add env setup (#2)
    * add app to docker-compose and use docker network host in connection string
    
    * clean up dockerfile
    
    * Create README.md
    
    * Create docker-publish.yml
    
    * add env setup
    
    * wip
    
    ---------
    
    Co-authored-by: Flemming Møller Pedersen <flemming@bjerk.io>
    Co-authored-by: Flemming Møller Pedersen <154523534+FlemmingMP@users.noreply.github.com>

--- keys.py
+++ keys.py
@@ -1,8 +1,14 @@
 # Dette er "keys.py"
 from sqlalchemy import create_engine, URL
+import os
+
+database_host = os.environ.get("DATABASE_HOST") or "localhost"
+database_password = os.environ.get("DATABASE_PASSWORD") or "Qwerty123"
+
+print("==== Connecting to ", database_host)
 
 # Tveit-instans, VB - SQLAlchemy:
-sql_connection_string_FPT_TV_SQL01_VB = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost,1433;DATABASE=master;UID=sa;PWD=Qwerty123;Encrypt=no"
+sql_connection_string_FPT_TV_SQL01_VB = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={database_host},1433;DATABASE=master;UID=sa;PWD={database_password};Encrypt=no"
 
 sql_connection_url_FPT_TV_SQL01_VB = URL.create(
     "mssql+pyodbc", query={"odbc_connect": sql_connection_string_FPT_TV_SQL01_VB})
